e0304c25db2e4b1de60efbd6f82d0292c4d95c59
vladimir <vladimirmuratov@mail.ru>
2023-03-18 18:15:20 +0300

    fix app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ if(process.env.NODE_ENV === 'production') {
 
     app.get('*', (req, res) => {
         res.sendFile(
-            path.resolve(__dirname, 'client', 'dist', 'index.html')
+            path.resolve(__dirname, 'client', 'build', 'index.html')
         )
     })
 }
