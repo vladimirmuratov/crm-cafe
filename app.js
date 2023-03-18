bada9fb77ac9bfe779aa8ae83c8e0fcf9910501f
vladimir <vladimirmuratov@mail.ru>
2023-03-18 18:35:59 +0300

    fix build

--- app.js
+++ app.js
@@ -36,12 +36,10 @@ app.use('/api/order', orderRoutes)
 app.use('/api/position', positionRoutes)
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/dist'))
+    app.use(express.static(path.join(__dirname, 'client/build')))
 
-    app.get('*', (req, res) => {
-        res.sendFile(
-            path.resolve(__dirname, 'client', 'build', 'index.html')
-        )
+    app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
