7e4c1b8690073b4be6ae3e5192b3cc09655c5049
vladimir <vladimirmuratov@mail.ru>
2023-03-18 19:13:58 +0300

    change keys

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,10 @@
-/*if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     module.exports = require('./keys.prod')
 } else {
     module.exports = require('./keys.dev')
-}*/
+}
 
-module.exports = {
+/*module.exports = {
     mongoURI: 'mongodb+srv://admin:admin@cluster0.swkdqse.mongodb.net/?retryWrites=true&w=majority',
     jwt: 'dev-jwt'
-}
+}*/
